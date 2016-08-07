f2b14a8a1aa37c9b5c14e0ce14769b59330f3586
Moshe Weiss <moman5757@gmail.com>
2016-08-07 17:22:11 -0400

    fixed user index and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :locations
-  root 'pages#home'
+  root 'users#index'
   devise_for :users
   get 'locations/new' => 'locations#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/:id' => 'users#show'
+  get 'users/index'
 end
