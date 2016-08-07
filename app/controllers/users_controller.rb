b0087b33c6c6878db65119279907dcb580703cb2
Moshe Weiss <moman5757@gmail.com>
2016-08-07 19:54:39 -0400

    user show and update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,34 @@
 class UsersController < ApplicationController
-  def index
-    @users = User.all
-  end
-  def show
-    @user = User.find(params[:id])
-  end
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+    def index
+      @users = User.all
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to @user, notice: 'User was successfully updated.' }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
+    end
+private
+
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :zip, :address, :country, :dob, :fb, :psn, :xbox, :steam, :twitch, :bio)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
   resources :locations
   root 'users#index'
   devise_for :users
+  resources :users
   get 'locations/new' => 'locations#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'users/:id' => 'users#show'
   get 'users/index'
+  get 'users/:id/edit' => 'users#edit'
 end
