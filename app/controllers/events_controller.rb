42932b4500d5854696ae8c19cd9905a8922b4fa9
Moshe Weiss <moman5757@gmail.com>
2016-08-08 17:31:59 -0400

    added events controller and views

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,53 @@
+class EventsController < ApplicationController
+  before_action :set_event, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: 'Event was successfully created.' }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+  end
+
+  def delete
+  end
+
+  def edit
+  #if current_user is host
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: 'Event was successfully updated.' }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+    private
+  def event_params
+    params.require(:event).permit(:name, :description, :date, :time, :address, :city, :state, :zip, :country, :host)
+  end
+  def set_event
+    @event = Event.find(params[:id])
+  end
+end
