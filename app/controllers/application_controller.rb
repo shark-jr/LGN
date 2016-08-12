08250362f76dce4bb813389ac844c10c72b53294
Moshe Weiss <moman5757@gmail.com>
2016-08-12 16:12:17 -0400

    Added zip to registration

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up) do |user_params|
-      user_params.permit( :email, :password, :password_confirmation, :username )
+      user_params.permit( :email, :password, :password_confirmation, :username, :zip )
     end
 
     devise_parameter_sanitizer.permit(:account_update) do |user_params|
