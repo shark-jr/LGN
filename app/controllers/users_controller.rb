e9bfd634a7ec2d05422352b0d2d1d10252ae63a5
Moshe Weiss <moman5757@gmail.com>
2016-08-11 21:44:29 -0400

    removed extra auth from usercont

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
 
     def index
       @users = current_user.nearbys(2000) || User.near("Miami", 200)
