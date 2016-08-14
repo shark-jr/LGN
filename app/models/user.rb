22c11a740032dcf914c0214e7530fdc1cfc268fe
Moshe Weiss <moman5757@gmail.com>
2016-08-14 17:31:38 -0400

    Profile links and persistent state on edit

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,4 +26,5 @@ class User < ApplicationRecord
   geocoded_by :zip
   after_save :geocode
   after_validation :geocode
+    STATES = ["Alabama", "Alaska", "Arizona", "Arkansas", "California", "Colorado", "Connecticut", "Delaware", "District of Columbia", "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming"]
 end
