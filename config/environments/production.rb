1b65068bd8efd94b22d27347b79a1c9b2be3baca
MiguelDotL <miguel.d.lozano@gmail.com>
2016-08-14 13:15:30 -0400

    removed actioncable config from production.rb / heroku application error fixed?

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,8 +37,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = 'wss://lgnet.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://lgnet.herokuapp.com', /http:\/\/example.*/ ]
+  # config.action_cable.url = 'wss://lgnet.herokuapp.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'https://lgnet.herokuapp.com', /http:\/\/example.*/ ]
   # config.web_socket_server_url = "wss://lgnet.herokuapp.com/"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
